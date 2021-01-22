d778abd79d41526c7ec58b901b460e8f44e9e975
nvtslot <nvtslot@live.nl>
2021-01-22 16:08:05 +0100

    Cleaned up the code

--- tagtical_insertion/pipeline/evaluation.py
+++ tagtical_insertion/pipeline/evaluation.py
@@ -85,18 +85,11 @@ def label_scores(y_given, y_predicted):
 
 def main():
 	#load the created model
-	#output_dir = 'model/'
 	output_dir = "model/"
-	#output_dir = '/home/tux/Computational-Semantics/tagtical_insertion/pipeline/model/'
-	
-	#not necessary I think
-	print("Loading from", output_dir)
 	
 	#load the data you want to test on
 	nlp = spacy.load(output_dir)
-	#corpus = extract_from_conll("../../data/dev.conll")
-	corpus = extract_from_conll("../../data/dev.conll")
-	#corpus = extract_from_conll("/home/tux/Computational-Semantics/data/train.conll")
+	corpus = extract_from_conll("../../data/test.conll")
 	data = to_spacy_format(corpus)
 	
 	#create a list that will keep track of the original tags and predicted tags
@@ -109,6 +102,7 @@ def main():
 	wrong_token = 0
 	no_entities = 0
 	
+	# These lists are used to fill in evaluation.txt
 	list_of_empty_sentences = []
 	list_of_sentences_with_wrong_tokens = []
 	list_of_sentences_with_incorrect_tags = []
@@ -153,7 +147,8 @@ def main():
 				y_given.append(label)
 			
 		cnt +=1
-
+	
+	# Create evaluation.txt and write all results and error analysis in it
 	correct = cnt - wrong_token - no_entities
 	with open('evaluation.txt', 'w') as f:
 		f.write("We evaluated the model on the file dev.conll")
@@ -197,56 +192,8 @@ def main():
 			f.write("\n")
 			f.write("\n")					
 		
-
+	# Finally, run the function that calculates scores per label and writes it in label_scores.txt
 	label_scores(y_given, y_predicted)
-
-	#print(cnt)
-	
-	#print(wrong_token)
-	#print(list_of_sentences_with_wrong_tokens)
-	
-	#print(no_entities)
-	#print(list_of_empty_sentences)
-	
-	#print(len(y_given))
-	#print(len(y_predicted))
-	#print(list_of_sentences_with_incorrect_tags[1])
-	#print(incorrect_tags_original[1])
-	#print(incorrect_tags_predicted[1])
-
-	#from sklearn.metrics import f1_score
-	#print(f1_score(y_given,y_predicted, average='micro'))
-	#for item in real:
-	#	print(item[2])
-	#print(real[1])
-	#print(y_given)
-	#print(correct_token)
-	#print(cnt)
-	#print(no_entities)
-		
-	#print(train_data)
-	#print(train_data[2])
-	#for i in range(len(train_data)):
-	#	print(train_data[i][1])
-
-	#x = train_data[6][1]
-	#x = x.get('entities')
-	#print(x)
-	#print(len(x))
-
-	#print(train_data[2])
-	#text = 'She likes short skirts.'
-	#doc=nlp2(text)
-	#for ent in doc.ents:
-	#	print(ent.label_)
-	#print("Entities", [(ent.text, ent.label_) for ent in doc.ents])
-	#print("Tokens", [(t.text, t.ent_type_, t.ent_iob) for t in doc])
-
-	#text gives the full sentences like "Tom ripped out the page"
-	#for text, _ in train_data:
-		#doc = nlp2(text)
-		#print("Entities", [(ent.text, ent.label_) for ent in doc.ents])
-		#print("Tokens", [(t.text, t.ent_type_, t.ent_iob) for t in doc])
 
 if __name__ == '__main__':
 	main()
